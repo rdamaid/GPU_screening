d8ac2cdeeaae7a0ce32b089d5de0e9f4967ae4b3
Refianto Damai Darmawan <34114125+rdamaid@users.noreply.github.com>
2022-08-01 22:56:05 +0700

    Add files via upload

---
+++ cek.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main() {
+    int a = 2, b = 3;
+    std::cout << (float(a)/float(b)) << std::endl;
+   if (__cplusplus == 201703L)
+      std::cout << "C++17" << std::endl;
+   else if (__cplusplus == 201402L)
+      std::cout << "C++14" << std::endl;
+   else if (__cplusplus == 201103L)
+      std::cout << "C++11" << std::endl;
+   else if (__cplusplus == 199711L)
+      std::cout << "C++98" << std::endl;
+   else
+      std::cout << "pre-standard C++" << std::endl;
+}
